441bc6fe2c4188ebfa41da000e8376fb69345234
Virappan-ops <virappan22@gmail.com>
2026-04-16 21:21:02 +0530

    Exp 6 and 7 Done

--- Experiment-7/src/main/java/com/charu/experiment7/ResourceController.java
+++ Experiment-7/src/main/java/com/charu/experiment7/ResourceController.java
@@ -1,6 +1,7 @@
 package com.charu.experiment7;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ResourceController {
 
     @GetMapping("/open/info")
--- Experiment-7/src/main/java/com/charu/experiment7/SecuritySetup.java
+++ Experiment-7/src/main/java/com/charu/experiment7/SecuritySetup.java
@@ -21,6 +21,7 @@ public class SecuritySetup {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/open/**").permitAll()
